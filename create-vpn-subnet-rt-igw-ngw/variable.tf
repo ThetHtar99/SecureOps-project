4a5b2d9c27877be259243abd0bf13236c54d18ac
Thet <thethtarswezin0909@gmail.com>
2024-10-01 09:46:16 +0000

    update variable

--- create-vpn-subnet-rt-igw-ngw/variable.tf
+++ create-vpn-subnet-rt-igw-ngw/variable.tf
@@ -4,7 +4,7 @@ variable "public_subnet_01" {
 }
 
 variable "az_1a" {
-    default = "ap_southeast_1a"
+    default = "ap-southeast-1a"
     description = "availability_zone"
   
 }
@@ -15,7 +15,7 @@ variable "public_subnet_02" {
 }
 
 variable "az_1b" {
-    default = "ap_southeast_1b"
+    default = "ap-southeast-1b"
     description = "availability_zone"
   
 }
@@ -26,7 +26,7 @@ variable "public_subnet_03" {
 }
 
 variable "az_1c" {
-    default = "ap_southeast_1c"
+    default = "ap-southeast-1c"
     description = "availability_zone"
   
 }
