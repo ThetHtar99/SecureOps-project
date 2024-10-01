26d703a1f0b3b45f93ba22d2e65d5dc36555dd5e
Thet <thethtarswezin0909@gmail.com>
2024-10-01 10:39:42 +0000

    vpc subnet 6

--- create-vpn-subnet-rt-igw-ngw/variable.tf
+++ create-vpn-subnet-rt-igw-ngw/variable.tf
@@ -1,32 +1,4 @@
-variable "public_subnet_01" {
-    default = "10.10.1.0/24"
-    description = "public_subnet_01"
-}
-
-variable "az_1a" {
-    default = "ap-southeast-1a"
-    description = "availability_zone"
-  
-}
-
-variable "public_subnet_02" {
-    default = "10.10.2.0/24"
-    description = "public_subnet_02"
-}
-
-variable "az_1b" {
-    default = "ap-southeast-1b"
-    description = "availability_zone"
-  
-}
-
-variable "public_subnet_03" {
-    default = "10.10.3.0/24"
-    description = "public_subnet_03"
-}
-
-variable "az_1c" {
-    default = "ap-southeast-1c"
-    description = "availability_zone"
-  
+variable "public_subnets" {
+    default = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+    description = "public_subnets"
 }
