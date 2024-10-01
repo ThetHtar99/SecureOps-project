6446f86200d63c7558b2e3a0ca22e8104494bcb5
Thet <thethtarswezin0909@gmail.com>
2024-10-01 09:41:15 +0000

    update main file

--- create-vpn-subnet-rt-igw-ngw/main.tf
+++ create-vpn-subnet-rt-igw-ngw/main.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public_subnet_01" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_01
   availability_zone = var.az_1a
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet-01-ap-southeast-1a"
@@ -25,10 +25,10 @@ resource "aws_subnet" "public_subnet_02" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_02
   availability_zone = var.az_1b
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-01-ap-southeast-1a"
+    Name = "public-subnet-01-ap-southeast-1b"
   }
 }
 
@@ -36,10 +36,10 @@ resource "aws_subnet" "public_subnet_03" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_03
   availability_zone = var.az_1c
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-01-ap-southeast-1a"
+    Name = "public-subnet-01-ap-southeast-1c"
   }
 }
 
